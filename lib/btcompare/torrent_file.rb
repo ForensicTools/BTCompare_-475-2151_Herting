8d172e30733c5072297e6f6bbe8265618b48df0c
Jennifer Herting <jen@herting.cc>
2015-11-08 10:48:51 -0500

    added attr readers

--- lib/btcompare/torrent_file.rb
+++ lib/btcompare/torrent_file.rb
@@ -5,6 +5,12 @@ require 'bencode'
 module BTCompare
 	class TorrentFile
 
+		# Raw data created parsed from the torrent file
+		attr_reader :data
+
+		# File name of the torrent file
+		attr_reader :filename
+
 		# @param filename [String] Path to torrent file
 		def initialize filename
 			@filename = filename
