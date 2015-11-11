ec2fca9c2d611b82c41300fc9f3e8f66a974ea33
Jennifer Herting <jen@herting.cc>
2015-11-11 11:19:10 -0500

    created array for storing piece ids of differences

--- lib/btcompare/result.rb
+++ lib/btcompare/result.rb
@@ -7,10 +7,15 @@ module BTCompare
 		attr_accessor :same_piece_count
 
 
+		# Contains piece ids of differences
+		attr_accessor :difference_ids
+
+
 		# @param parent [Comparison] Parent of the result
 		def initialize parent
 			@parent = parent
 			@same_piece_count = nil
+			@difference_ids = nil
 		end
 
 	end
