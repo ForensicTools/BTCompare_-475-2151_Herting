a30099093cadb9c1a024a7ce8ec477c0488b0360
Jennifer Herting <jen@herting.cc>
2015-11-11 11:13:53 -0500

    added same piece count

--- lib/btcompare/result.rb
+++ lib/btcompare/result.rb
@@ -3,10 +3,14 @@
 module BTCompare
 	class Result
 
+		# Files have same piece count
+		attr_accessor :same_piece_count
+
 
 		# @param parent [Comparison] Parent of the result
 		def initialize parent
 			@parent = parent
+			@same_piece_count = nil
 		end
 
 	end
