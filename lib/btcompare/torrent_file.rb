0f585fbd11dd9b94e25b6ba1b7f43665e31d634c
Jennifer Herting <jen@herting.cc>
2015-11-11 10:21:52 -0500

    fixed so 0x12 (?) isn't special

--- lib/btcompare/torrent_file.rb
+++ lib/btcompare/torrent_file.rb
@@ -29,7 +29,7 @@ module BTCompare
 				return @individual_peices
 			end
 			
-			@individual_peices = @data['info']['pieces'].scan /.{20}/
+			@individual_peices = @data['info']['pieces'].scan /.{1,20}/m
 
 			return @individual_peices 
 		end
