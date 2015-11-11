cb6047061b60c24a1287e881da201b788f78b1d4
Jennifer Herting <jen@herting.cc>
2015-11-11 11:11:20 -0500

    getting piece_count and formatting

--- lib/btcompare/torrent_file.rb
+++ lib/btcompare/torrent_file.rb
@@ -8,8 +8,10 @@ module BTCompare
 		# Raw data created parsed from the torrent file
 		attr_reader :data
 
+
 		# File name of the torrent file
 		attr_reader :filename
+
 
 		# @param filename [String] Path to torrent file
 		def initialize filename
@@ -19,6 +21,7 @@ module BTCompare
 
 			@individual_pieces = nil
 		end
+
 
 		# Generates individual pieces if not alreaded found.
 		# Caches data once created. Immediately returns array
@@ -32,6 +35,13 @@ module BTCompare
 			@individual_pieces = @data['info']['pieces'].unpack( "H*" ).first.scan /.{40}/m
 
 			return @individual_pieces 
+		end
+
+		
+		# Created based on ( length / piece length ).ceil
+		# @return [Fixnum] Number of peices
+		def piece_count 
+			return ( @data['info']['length'] / @data['info']['piece length'] ).ceil
 		end
 
 
