9001b9050ce195b0eeff6d712056fef5bfec3d72
Jennifer Herting <jen@herting.cc>
2015-11-11 09:54:00 -0500

    returning individual peices

--- lib/btcompare/torrent_file.rb
+++ lib/btcompare/torrent_file.rb
@@ -16,7 +16,22 @@ module BTCompare
 			@filename = filename
 			
 			@data = BEncode::load(File.open( filename, 'r' ))
+
+			@individual_peices = nil
+		end
+
+		# Generates individual peices if not alreaded found.
+		# Caches data once created. Immediately returns array
+		# if already generated.
+		# @return array of individual SHA1 hashes from torrent file
+		def individual_peices
+			unless @individual_peices == nil then
+				return @individual_peices
+			end
 			
+			@individual_peices = @data['info']['peices'].scan /.{20}/
+
+			return @individual_peices 
 		end
 
 
