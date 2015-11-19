118f8755cf6d48fa433ceac242f12abbc3ffb7c0
Jennifer Herting <jen@herting.cc>
2015-11-19 12:02:45 -0500

    creating tmp directory

--- lib/btcompare/diff/diff.rb
+++ lib/btcompare/diff/diff.rb
@@ -6,6 +6,17 @@ module BTCompare
 		# Parent class for Diff objects
 		class Diff
 
+			private
+
+			# Creates a temporary directory for storing diffs
+			# @return [String] Path for the directory
+			#   created.
+			def create_tmp_directory
+				path = "tmp.#{Random.new.rand(100000..999999)}"
+				Dir.mkdir path
+				return path
+			end
+
 		end
 	end
 end
