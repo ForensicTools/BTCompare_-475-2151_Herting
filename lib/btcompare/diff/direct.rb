2150dac5797f4bbbc51dfef019758bf1745d7fad
Jennifer Herting <jen@herting.cc>
2015-11-19 12:19:36 -0500

    defined constructor for BTCompare::Diff::Direct

--- lib/btcompare/diff/direct.rb
+++ lib/btcompare/diff/direct.rb
@@ -4,7 +4,13 @@ module BTCompare
 	module Diff
 
 		# Contains and process a direct diff
-		class Direct
+		class Direct 
+
+			# Creates a new Direct diff
+			def initialize parent
+				super parent
+
+			end
 
 		end
 	end
