606bce2ed719de310925482190f97312e0299c7c
Jennifer Herting <jen@herting.cc>
2015-11-19 13:54:03 -0500

    added logging for ease of debugging

--- lib/btcompare/diff/direct.rb
+++ lib/btcompare/diff/direct.rb
@@ -20,9 +20,19 @@ module BTCompare
 
 			# Processes the carving of the data
 			def process
+				log = BTCompare::LOG
+
+				log.info "Beginning to process a direct diff."
+				log.info "File 1: #{@torrent_file_1.filename}"
+				log.info "File 2: #{@torrent_file_2.filename}"
+
+				log.debug "Opening file 1"
 				source_1 = File.open @torrent_file_1.contains, 'rb'
+
+				log.debug "Opening file 2"
 				source_2 = File.open @torrent_file_2.contains, 'rb'
 				
+				log.debug "Making directories"
 				Dir.mkdir( File.join( @path, '1' ))
 				Dir.mkdir( File.join( @path, '1', 'bin' ))
 				Dir.mkdir( File.join( @path, '2' ))
@@ -30,24 +40,38 @@ module BTCompare
 
 				bin_files = []
 
+				log.debug "Beginning bin carving"
 				@parent.offsets do |id, offset|
 					chunk_path = File.join( @path, "1", "bin", id.to_s )
 					@created_files.push chunk_path
 					bin_files.push chunk_path
 					File.open( chunk_path, 'w' ) do |file|
-						carve source_1, offset, @torrent_file_1.piece_length, file
+						if carve( source_1, offset, @torrent_file_1.piece_length, file ) then
+							log.debug "Wrote #{offset} to #{chunk_path}"
+						else
+							log.warn "Carve has noticed difference in piece_length and written length"
+						end
 					end
 
 					chunk_path = File.join( @path, "2", "bin", id.to_s )
 					@created_files.push chunk_path
 					bin_files.push chunk_path
 					File.open( chunk_path, 'w' ) do |file|
-						carve source_2, offset, @torrent_file_2.piece_length, file
+						if carve( source_2, offset, @torrent_file_2.piece_length, file ) then
+							log.debug "Wrote #{offset} to #{chunk_path}"
+						else
+							log.warn "Carve has noticed difference in piece_length and written length"
+						end
 					end
 				end
+				log.debug "Finished with bin carving"
 
+				log.debug "Closing file 1"
 				source_1.close
+				log.debug "Closing file 2"
 				source_2.close
+
+				log.info "Direct diff processing finished" 
 			end
 		end
 	end
