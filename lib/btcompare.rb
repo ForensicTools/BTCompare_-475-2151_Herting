d71ddff8286adae9bcd6844e82b049ba6f7fed1c
Jennifer Herting <jen@herting.cc>
2015-11-19 13:31:02 -0500

    creating Syslog::Logger object

--- lib/btcompare.rb
+++ lib/btcompare.rb
@@ -1,4 +1,5 @@
 
+require 'syslog/logger'
 require_relative 'btcompare/torrent_file.rb'
 require_relative 'btcompare/comparison.rb'
 require_relative 'btcompare/result.rb'
@@ -7,6 +8,8 @@ require_relative 'btcompare/diff.rb'
 # Library used for comparing two different .torrent files.
 module BTCompare
 
+	# Syslog::Logger object for logging
+	LOG = Syslog::Logger.new 'btcompare'
 end
 
 
