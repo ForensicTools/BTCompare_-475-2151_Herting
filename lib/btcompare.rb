3c3994a48ccd6ff67223e47abcb388e81bb1d69f
Jennifer Herting <jen@herting.cc>
2015-11-19 11:41:54 -0500

    working on adding diff classes

--- lib/btcompare.rb
+++ lib/btcompare.rb
@@ -2,6 +2,7 @@
 require_relative 'btcompare/torrent_file.rb'
 require_relative 'btcompare/comparison.rb'
 require_relative 'btcompare/result.rb'
+require_relative 'btcompare/diff.rb'
 
 # Library used for comparing two different .torrent files.
 module BTCompare
---
+++ lib/btcompare/diff.rb
@@ -0,0 +1,11 @@
+
+require_relative 'diff/diff.rb'
+
+module BTCompare
+	
+	# Handles processing of diffs
+	module Diff
+
+	end
+end
+
