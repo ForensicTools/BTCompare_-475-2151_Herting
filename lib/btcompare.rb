3f8d3be25d241cd57fab8d58be2a63383088d829
Jennifer Herting <jen@herting.cc>
2015-11-19 11:27:02 -0500

    Started documentation for module BTCompare

--- lib/btcompare.rb
+++ lib/btcompare.rb
@@ -3,6 +3,7 @@ require_relative 'btcompare/torrent_file.rb'
 require_relative 'btcompare/comparison.rb'
 require_relative 'btcompare/result.rb'
 
+# Library used for comparing two different .torrent files.
 module BTCompare
 
 end
