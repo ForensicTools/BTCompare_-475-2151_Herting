93b254def3366bce475ed48e05f5361926467689
Jennifer Herting <jen@herting.cc>
2015-11-19 18:07:25 -0500

    requiring exception.rb

--- lib/btcompare.rb
+++ lib/btcompare.rb
@@ -4,6 +4,7 @@ require_relative 'btcompare/torrent_file.rb'
 require_relative 'btcompare/comparison.rb'
 require_relative 'btcompare/result.rb'
 require_relative 'btcompare/diff.rb'
+require_relative 'btcompare/exception.rb'
 
 # Library used for comparing two different .torrent files.
 module BTCompare
