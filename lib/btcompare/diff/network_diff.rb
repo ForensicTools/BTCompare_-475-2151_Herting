92d00ef6cdbb09b67c0292af8f44b9ea1e7eb88d
Jennifer Herting <jen@herting.cc>
2015-12-16 16:12:47 -0500

    created scaffolding for network_diff

---
+++ lib/btcompare/diff/network_diff.rb
@@ -0,0 +1,13 @@
+
+module BTCompare
+	module Diff
+
+		# Module included by BTCompare::Diff::Diff when 
+		# networkize method is run. Overwrites other
+		# methods to add jobs to the worker queue.
+		module NetworkDiff
+
+		end
+	end
+end
+
