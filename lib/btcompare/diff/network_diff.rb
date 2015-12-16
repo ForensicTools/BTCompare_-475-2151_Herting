66e0e2a96809603d20dd11618d81ea90ee460f40
Jennifer Herting <jen@herting.cc>
2015-12-16 16:18:02 -0500

    created scaffolding for carve

--- lib/btcompare/diff/network_diff.rb
+++ lib/btcompare/diff/network_diff.rb
@@ -7,6 +7,16 @@ module BTCompare
 		# methods to add jobs to the worker queue.
 		module NetworkDiff
 
+			# Creates a carve job in the master's job queue.
+			# @param [File] in_file File data is coming from
+			# @param [Integer] offset Starting offset
+			# @param [Integer] length Length of chunk
+			# @param [File] out_file File data is going to
+			# @todo Write method
+			def carve in_file, offset, length, out_file
+				return false
+			end
+
 		end
 	end
 end
