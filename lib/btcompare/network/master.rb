3bca0948b22d6904893629886c741d6efde1504d
Jennifer Herting <jen@herting.cc>
2015-12-16 15:17:30 -0500

    working on master.rb

--- lib/btcompare/network/master.rb
+++ lib/btcompare/network/master.rb
@@ -1,12 +1,33 @@
 
+require 'socket'
 
 module BTCompare
 	module Network
 
-		# Describes the Master node
+		# Describes the Master Server
 		class Master
 
+			# Array of worker nodes.
+			attr_reader :workers
 
+			# Creates a new Master Server
+			# @option opts [Integer] :port (5643) Port to listen on
+			def initialize opts = {}
+				# Set defaults
+				opts[:port] = 5643 unless opts.has_key? :port
+
+				# Define default instance variables
+				@workers = []
+
+				# Open server
+				@server = TCPServer.open opts[:port]
+
+				Thread.start do 
+					client = @server.accept
+				end
+
+
+			end
 		end
 	end
 end
