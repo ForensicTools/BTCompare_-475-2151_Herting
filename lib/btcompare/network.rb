75a870ca2dd20924fa03ac6055a41737bf0534d5
Jennifer Herting <jen@herting.cc>
2015-12-16 15:29:58 -0500

    added requires

--- lib/btcompare/network.rb
+++ lib/btcompare/network.rb
@@ -1,4 +1,8 @@
 
+require_relative 'network/worker.rb'
+require_relative 'network/worker_connection.rb'
+require_relative 'network/master.rb'
+
 module BTCompare
 
 	# Module for handling networked workers
