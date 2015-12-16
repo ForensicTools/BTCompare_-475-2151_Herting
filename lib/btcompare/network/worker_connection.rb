76b90ed26fa4f88c743aac278a7c8ececec6ba22
Jennifer Herting <jen@herting.cc>
2015-12-16 15:25:13 -0500

    working on constructor

--- lib/btcompare/network/worker_connection.rb
+++ lib/btcompare/network/worker_connection.rb
@@ -5,6 +5,17 @@ module BTCompare
 		# Master Side description of a worker
 		class WorkerConnection
 
+			# Creates a master side representation of a worker
+			# @param [BTCompare::Network::Master] parent Master that owns this worker
+			# @param [TCPSocket] socket Connection to the worker
+			def initialize parent, socket
+				@parent = parent
+				@socket = socket
+				
+				@thread = Thread.new do
+
+				end
+			end
 		end
 	end
 end
